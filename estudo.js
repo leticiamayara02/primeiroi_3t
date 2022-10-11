ebaf7d69c2cd3574352dcef79f29ab5f8664e47b
leticiamayara02 <110063077+leticiamayara02@users.noreply.github.com>
2022-10-11 17:51:16 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,24 +1,7 @@
 // alert("Oi! Eu sou o Alert");
 // document.write("Estou na página.");
 // console.log("Eu estou no console.");
-let tabuada = 4
-function alo(){
-    document.write("<h1>Tabuada do " + tabuada + "</h1>")
-    document.write( tabuada + " x 1 = " + (tabuada *  1)+"<br>");
-    document.write( tabuada + " x 2 = " + (tabuada *  2)+"<br>");
-    document.write( tabuada + " x 3 = " + (tabuada *  3)+"<br>");
-    document.write( tabuada + " x 4 = " + (tabuada *  4)+"<br>");
-    document.write( tabuada + " x 5 = " + (tabuada *  5)+"<br>");
-    document.write( tabuada + " x 6 = " + (tabuada *  6)+"<br>");
-    document.write( tabuada + " x 7 = " + (tabuada *  7)+"<br>");
-    document.write( tabuada + " x 8 = " + (tabuada *  8)+"<br>");
-    document.write( tabuada + " x 9 = " + (tabuada *  9)+"<br>");
-    document.write( tabuada + " x 10 = " + (tabuada *  10)+"<br>");
-}
-// alert("Oi! Eu sou o Alert");
-// document.write("Estou na página.");
-// console.log("Eu estou no console.");
-let tabuada = 4;
+let tabuada = 5;
 function alo(){
 
     let tabuada = document.getElementById("entrada").value;
@@ -50,10 +33,36 @@ function quadrado(){
         document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
     }
 }
-
 function total(){
-     let val = document.getElementById("valor").value;
-     let ju = document.getElementById("juros").value;
-     let resultado = (val * ((ju/100))+1));
-     document.write("O total é de: " + resultado);
+    let val = document.getElementById("valor").value;
+    let ju = document.getElementById("juros").value;
+    let resultado = (val * ((ju/100)+1));
+    document.write("O total é de: " + resultado);
+}
+// Para dividir usa a /
+// Calcular a média das 4 notas. Somas as 4 notas e divide por 4.
+// Quanto falta para passar. Pega 240 e subtrai as notas.
+function soma(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = Number(n1) + Number(n2) + Number(n3) + Number(n4);
+    document.getElementById("resultado").innerHTML = r;
+}
+function media(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = (Number(n1) + Number(n2) + Number(n3) + Number(n4));
+    document.getElementById("resultado").innerHTML = r;
+}
+function necessario(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    let r = 240 -  (Number(n1) + Number(n2) + Number(n3) + Number(n4));
+    document.getElementById("resultado").innerHTML = r;
 }
