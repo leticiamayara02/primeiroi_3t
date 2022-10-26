3f300b328852b96a6bf438c642bbe27b79b90930
leticiamayara02 <110063077+leticiamayara02@users.noreply.github.com>
2022-10-26 17:40:44 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -34,18 +34,23 @@ function quadrado(){
     }
 }
 function moeda(atual){
-    return atual.toLocaleString('pt-br',(style: 'currency', currency: 'BRL'});
-    
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
 function total(){
     let val = document.getElementById("valor").value;
     let ju = document.getElementById("juros").value;
     
     let t = document.getElementById("meses").value;
     let resultado = 0;
+    let saida = "";
     for(let m = 1; m <= t; m++){
         resultado = (val * ((ju/100)+1));
         val = resultado;
-        document.write("Mês " + m + " valor de " + val + "<br>");
+          saida += "Mês " + m + ": " + moeda(val) + "<br>";
+        //document.write("Mês " + m + " valor de " + moeda(val) + "<br>");
     }
-    document.write("O total é de: " + resultado);
+    document.getElementById("mes").innerHTML = saida;
+    document.getElementById("resultado").innerHTML = "Total: " + moeda(resultado);
+    // document.write("O total é de: " + moeda(resultado) );
 }
+
